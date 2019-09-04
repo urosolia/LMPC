c1f4860c8acf9bea5d588383e3bce84bf2acf323
Edward Zhu <zhu.edward445@gmail.com>
2019-09-04 13:00:22 -0700

    Add parallelization to initial trajectory

--- CoopLMPC/main.py
+++ CoopLMPC/main.py
@@ -2,6 +2,7 @@ import numpy as np
 from FTOCP import FTOCP
 from LMPC import LMPC
 import pdb
+import multiprocessing as mp
 
 import matplotlib
 matplotlib.use('TkAgg')
@@ -12,7 +13,45 @@ rc('text', usetex=True)
 import copy
 import pickle
 
-import os, sys
+import os, sys, time
+
+def init_traj(ftocp, x0, waypt, xf):
+	n_x = ftocp.n
+	n_u = ftocp.d
+
+	xcl_feas = x0
+	ucl_feas = np.empty((n_u,1))
+
+	mode = 1
+	t = 0
+	# time Loop (Perform the task until close to the origin)
+	while True:
+		xt = xcl_feas[:,t] # Read measurements
+
+		if mode == 1:
+			xg = waypt.reshape((n_x))
+			tol = 0
+		else:
+			xg = xf.reshape((n_x))
+			tol = 3
+
+		ftocp.solve(xt, xf=xg, verbose=0) # Solve FTOCP
+
+		# Read input and apply it to the system
+		ut = ftocp.get_u_opt()[:,0].reshape((n_u, 1))
+		xtp1 = ftocp.model(xt.reshape((n_x, 1)), ut)
+		ucl_feas = np.append(ucl_feas, ut, axis=1)
+		xcl_feas = np.append(xcl_feas, xtp1, axis=1)
+
+		if np.linalg.norm(xtp1-xg.reshape((n_x, 1)), ord=2)**2 <= 10**(-tol):
+			if mode == 1:
+				mode += 1
+			else:
+				break
+
+		t += 1
+
+	return (xcl_feas, ucl_feas)
 
 def main():
 	# Number of agents
@@ -56,9 +95,9 @@ def main():
 	if R.shape[0] != R.shape[1] or len(np.diag(R)) != n_u:
 		raise(ValueError('Q matrix not shaped properly'))
 
-	# Initialize FTOCP object
+	# Initialize FTOCP objects
 	N_feas = 10
-	ftocp  = [FTOCP(N_feas, A[i], B[i], 0.1*Q, R, Hx=Hx, gx=gx, Hu=Hu, gu=gu) for i in range(n_a)]
+	ftocp = [FTOCP(N_feas, A[i], B[i], 0.1*Q, R, Hx=Hx, gx=gx, Hu=Hu, gu=gu) for i in range(n_a)]
 
 	# ====================================================================================
 	# Run simulation to compute feasible solutions for all agents
@@ -67,47 +106,32 @@ def main():
 	plot_init = True
 	waypt = [np.array([[2, 1.5, 0, 0]]).T, np.array([[0, -1.5, 0, 0]]).T]
 
-	xcl_feas = x0
-	ucl_feas = [np.empty((n_u,1)) for _ in range(n_a)]
+	xcl_feas = []
+	ucl_feas = []
 
-	for i in range(n_a):
-		mode = 1
-		print('Solving for initial feasible trajectory for agent %i' % (i+1))
-		t = 0
-		# time Loop (Perform the task until close to the origin)
-		while True:
-			xt = xcl_feas[i][:,t] # Read measurements
+	parallel = False
 
-			if mode == 1:
-				xg = waypt[i].reshape((n_x))
-				tol = 0
-			else:
-				xg = xf[i].reshape((n_x))
-				tol = 3
+	start = time.time()
+	if parallel:
+		pool = mp.Pool(processes=n_a)
 
-			ftocp[i].solve(xt, xf=xg, verbose = 0) # Solve FTOCP
+		results = [pool.apply_async(init_traj, args=(ftocp[i], x0[i], waypt[i], xf[i])) for i in range(n_a)]
+		init_trajs = [r.get() for r in results]
 
-			# Read input and apply it to the system
-			ut = ftocp[i].get_u_opt()[:,0].reshape((n_u, 1))
-			xtp1 = ftocp[i].model(xt.reshape((n_x, 1)), ut)
-			ucl_feas[i] = np.append(ucl_feas[i], ut, axis=1)
-			xcl_feas[i] = np.append(xcl_feas[i], xtp1, axis=1)
+		(xcl_feas, ucl_feas) = zip(*init_trajs)
+	else:
+		for i in range(n_a):
+			(x, u) = init_traj(ftocp[i], x0[i], waypt[i], xf[i])
+			xcl_feas.append(x)
+			ucl_feas.append(u)
+	end = time.time()
 
-			if np.linalg.norm(xtp1-xg.reshape((n_x, 1)), ord=2)**2 <= 10**(-tol):
-				if mode == 1:
-					mode += 1
-				else:
-					break
-
-			t += 1
-
-		print('Agent %i trajectory length: %i' % (i+1, ucl_feas[i].shape[1]))
-		# print np.round(np.array(xcl_feas[i]).T, decimals=2)
-		# print np.round(np.array(ucl_feas[i]).T, decimals=2)
+	print('Time elapsed: %g s' % (end - start))
 
 	if plot_init:
 		plt.figure(1)
 		for i in range(n_a):
+			print('Agent %i trajectory length: %i' % (i+1, ucl_feas[i].shape[1]))
 			c = matplotlib.cm.get_cmap('jet')(i*(1./(n_a-1)))
 			plt.plot(xcl_feas[i][0,:], xcl_feas[i][1,:], '.', c=c)
 			plt.xlabel('$x_1$')
@@ -121,11 +145,12 @@ def main():
 	# Run LMPC
 	# ====================================================================================
 
-	# Initialize LMPC object
-	N_LMPC = 4 # horizon length
-	ftocp_for_lmpc = FTOCP(N_LMPC, A, B, Q, R) # ftocp solve by LMPC
-	lmpc = LMPC(ftocp_for_lmpc, CVX=True) # Initialize the LMPC decide if you wanna use the CVX hull
-	lmpc.addTrajectory(xcl_feasible, ucl_feasible) # Add feasible trajectory to the safe set
+	# Initialize LMPC objects for each agent
+	N_LMPC = [4, 4] # horizon length
+	ftocp_for_lmpc = [FTOCP(N_LMPC[i], A[i], B[i], Q, R, Hx=Hx, gx=gx, Hu=Hu, gu=gu) for i in range(n_a)]# ftocp solve by LMPC
+	lmpc = [LMPC(f, CVX=True) for f in ftocp_for_lmpc]# Initialize the LMPC decide if you wanna use the CVX hull
+	for l in lmpc:
+		l.addTrajectory(xcl_feas[i], ucl_feas[i]) # Add feasible trajectory to the safe set
 	totalIterations = 10 # Number of iterations to perform
 
 	# run simulation
@@ -134,18 +159,18 @@ def main():
 		xcl = [x0] # initialize system state at interation it
 		ucl =[]
 		xt = x0
-		time = 0
+		t = 0
 		# time Loop (Perform the task until close to the origin)
 		while np.dot(xt, xt) > 10**(-10):
-			xt = xcl[time] # Read measurements
+			xt = xcl[t] # Read measurements
 
 			lmpc.solve(xt, verbose = 0) # Solve FTOCP
 
 			# Read input and apply it to the system
 			ut = lmpc.uPred[:,0][0]
 			ucl.append(copy.copy(ut))
-			xcl.append(copy.copy(lmpc.ftocp.model(xcl[time], ut)))
-			time += 1
+			xcl.append(copy.copy(lmpc.ftocp.model(xcl[t], ut)))
+			t += 1
 
 		# print np.round(np.array(xcl).T, decimals=2) # Uncomment to print trajectory
 		# Add trajectory to update the safe set and value function
